4032d9eb0f4710513d7d5d1301e31d8656f557e5
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-09-03 19:29:21 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int count = 0;
+        
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i] < 0){
+                answer = i;
+                count++;
+                break;
+            }
+        }
+        
+        if(count == 0){
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
